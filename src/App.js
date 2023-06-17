f70db5d5c49b3c2ffb7b43682d243cfb6498fca9
Pujitha <rajeswaripujitha@gmail.com>
2023-06-17 19:54:02 +0530

    al

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <form >
+      <h1>Admission Form</h1>
+      <h3>Enter your details carefully</h3>
+       <label> Name:
+        <input type="text" placeholder='type your name'  />
+      </label><br/>
+      <label>Email:
+        <input type="text" placeholder='aaaaa@gmail.com' />
+      </label><br/>
+      <label>Mobile number :
+        <input type="text" placeholder='+91 0000000000' />
+      </label><br/>
+      <label>Branch :
+        <input type="text" placeholder='your branch'  />
+      </label><br/>
+      <input type="button" value='submit'/>
+    </form>
+    </>
   );
 }
 
